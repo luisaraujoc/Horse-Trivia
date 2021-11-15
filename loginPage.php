b2039df082dbe968acd2122e0065c17d3d739761
Luís Henrique Couto <luislk_@outlook.com>
2021-11-15 09:41:28 -0300

    Olá Mundo

--- loginPage.php
+++ loginPage.php
@@ -1,47 +1,64 @@
+<?php
+include 'sys/conx.php';
 
-    <!DOCTYPE html>
-    <html lang="en">
 
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- LINKs -->
-        <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
-        <link rel="stylesheet" href="./node_modules/nes.css/css/nes.min.css">
-        <!-- <link rel="stylesheet" href="node_modules/nes.css/css/nes.css"> -->
-        <link rel="stylesheet" href="css/style.css">
-    </head>
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-    <body class="align">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINKs -->
+    <link rel="stylesheet" href="./node_modules/nes.css/css/nes.min.css">
+    <!-- <link rel="stylesheet" href="node_modules/nes.css/css/nes.css"> -->
+    <script src="https://kit.fontawesome.com/e15fd69637.js" crossorigin="anonymous"></script>
+    <script src="js/register.js"></script>
+    <link rel="stylesheet" href="css/register.css">
+</head>
 
-        <div class="login">
-
-            <header class="login__header">
-                <h2>Entrar</h2>
-            </header>
-
-            <form action="login.php" class="login__form" method="POST">
-
-                <div>
-                    <label for="email">Nome de Usuário:</label>
-                    <input type="text" id="username" name="username" placeholder="username" class='nes-input'>
+<body>
+    <section>
+        <div class="container">
+            <div class="user signinBx">
+                <div class="imgBx">
+                    <img src="img/pista.png" alt="" />
                 </div>
-
-                <div>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" placeholder="Senha" class='nes-input'>
+                <div class="formBx">
+                    <form action="" onsubmit="return false;">
+                        <h2>Entrar</h2>
+                        <input type="text" name="username" placeholder="Nome de Usuário" />
+                        <input type="password" name="password" placeholder="Senha" />
+                        <input type="submit" name="" value="Entrar" />
+                        <p class="signup">
+                            Não tem uma conta?
+                            <a href="#" onclick="toggleForm();">Crie uma conta.</a>
+                        </p>
+                    </form>
                 </div>
-
-                <div class="btn-div">
-                    <input class="nes-btn" type="submit" name="btn_login" value="Entrar">
-                    <input class="nes-btn is-success" name="btn_register" type="submit" value="Registrar">
-                    
+            </div>
+            <div class="user signupBx">
+                <div class="formBx">
+                    <form action="" onsubmit="return false;">
+                        <h2>Criando conta</h2>
+                        <input type="text" name="Username" placeholder="Nome de Usuário" />
+                        <input type="email" name="email" placeholder="Seu E-Mail" />
+                        <input type="password" name="password" placeholder="Senha" />
+                        <input type="password" name="confpass" placeholder="Confirme sua senha" />
+                        <input type="submit" name="" value="Criar Conta" />
+                        <p class="signup">
+                            Já tem uma conta?
+                            <a href="#" onclick="toggleForm();">Entrar.</a>
+                        </p>
+                    </form>
                 </div>
-
-            </form>
-
+                <div class="imgBx">
+                    <img src="img/pista.png" alt="" />
+                </div>
+            </div>
         </div>
+    </section>
+</body>
 
-    </body>
 
-    </html>
+</html>
--- sys/conx.php
+++ sys/conx.php
@@ -1,7 +1,29 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $db = "horsetrivia_db";
 
+class Database{
+
+    private $host = 'localhost';
+    private $dbname = 'horsetrivia_db';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
+
+
+    public function dbConnection()
+        {
+        
+            $this->conn = null;    
+            try
+            {
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+            }
+            catch(PDOException $exception)
+            {
+                echo "Connection error: " . $exception->getMessage();
+            }
+            
+            return $this->conn;
+        }
+    }    
 ?>
